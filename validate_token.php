c3b6f4c168e5a6ca3b217d24124bf4a6c9417e42
Dinusha2017 <dinushagammanpila@gmail.com>
2018-10-01 17:35:17 +0530

    Initial Commit

---
+++ validate_token.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && 
+    isset($_COOKIE["PHPSESSID"]) && isset($_COOKIE["CSRF_TOKEN"]) && 
+    isset($_POST['csrf_token'])) {
+
+    if($_POST['csrf_token'] == $_COOKIE["CSRF_TOKEN"]){
+        echo    "<script type='text/javascript'>
+                    alert(\"Operation successful\");
+                </script>";
+    }
+    else{
+        echo    "<script type='text/javascript'>
+                    alert(\"Operation failed\");
+                </script>";
+    }
+}
+else{
+    if(!(isset($_POST['csrf_token']))){
+        echo "token not set";
+    }
+}
+
+?>
